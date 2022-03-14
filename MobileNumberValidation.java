cd38b9d020b9f2f94ed9eb1e414522212de35173
veeresh <veereshyalawar2@gmail.com>
2022-03-14 20:27:23 +0530

    added uc4 files

---
+++ MobileNumberValidation.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileNumberValidation {
+
+public static void main(String[] args) {
+		/*
+		 * 
+		 *#####################################################
+		 *  1.Taking user input of first name by using scanner
+		 *  3.calling method to print to print results
+		 *  2.Here we created a method which checks whether the name starts with capital letter or not
+	     *    and have at least 3 letters
+		 *#####################################################
+		 *
+		 */
+
+		/*
+		 * 
+		 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *1.Taking user input of first name And Last name by using scanner
+		 *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Give space between country code and mobile number../n");
+		System.out.println("Enter the mobile number starting with country code: ");
+		String MobileNo = sc.nextLine();
+		
+	
+
+		/*
+		 *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *3.calling method to print the result
+		 *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *
+		 */
+		System.out.println(mobile(MobileNo));
+		
+		
+	}
+
+	    /*
+	     *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	     *2.Here we created a method which checks whether the First name and Last name starts with capital letter or not
+	     *  and have at least 3 letters 
+	     *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+     	 *
+	     */
+public static boolean mobile(String mobile) {
+	
+	return mobile.matches("[0-9]{2}\s[0-9]{10}");
+	
+    }
+}
